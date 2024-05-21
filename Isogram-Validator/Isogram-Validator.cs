5cbdfb2ada922de70a30ec74c06ac7fad533e847
Famez1 <a.plyakin@sh.dunice.net>
2024-05-21 11:42:37 +0300

    Сделал метод делающий из слова изограмму

---
+++ Isogram-Validator/Isogram-Validator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Isogram_Validator
+{
+    internal class Isogram_Validator
+    {
+        string RemoveDuplicates(string text)
+        {
+            HashSet<char> uniqueChars = new HashSet<char>();
+            StringBuilder stringBuilder = new StringBuilder();
+
+            foreach (char c in text.ToLower())
+            {
+                if (!uniqueChars.Contains(c))
+                {
+                    stringBuilder.Append(c);
+                    uniqueChars.Add(c);
+                }
+            }
+
+            return stringBuilder.ToString();
+        }
+
+    }
+}
